a769a51db7ce0b8abd658542d9ade5cf333ccd92
swimja10 <swimja10@gmail.com>
2026-03-16 19:22:17 -0500

    Refactored the oanda_account_details so that they are using the standard v20 API rather than the oandapy

--- oanda_account_details.py
+++ oanda_account_details.py
@@ -1,13 +1,19 @@
-import config
-from config import OANDA_ACCOUNT_ID, client
-import oandapyV20.endpoints.accounts as accounts
+import requests
+from config import OANDA_ACCOUNT_ID, OANDA_PRACTICE_API, headers
 
-def get_trade_count():
-    r = accounts.AccountDetails(OANDA_ACCOUNT_ID)
-    openTrade = client.request(r)['account']['openPositionCount']
-    return openTrade
+def get_oanda_open_trade_count():
+    url = OANDA_PRACTICE_API + f"/v3/accounts/{OANDA_ACCOUNT_ID}/trades"
+    query = {
+        "state": "OPEN"
+    }
+    response = requests.get(url, headers=headers, params=query)
+    json_response = response.json()
+    return len(json_response['trades'])
 
-def get_account_balance():
-    r = accounts.AccountDetails(OANDA_ACCOUNT_ID)
-    raw_balance = client.request(r)['account']["balance"]
-    return float(raw_balance)
+def get_oanda_account_balance():
+    url = OANDA_PRACTICE_API + f"/v3/accounts/{OANDA_ACCOUNT_ID}/summary"
+    response = requests.get(url, headers=headers)
+    json_response = response.json()
+    return json_response['account']["balance"]
+
+print(get_oanda_open_trade_count())
